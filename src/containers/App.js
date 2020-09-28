442e1bbb6b39eaa5c4e950b109875ae7cf2c01e4
Adrian Lim <adrianlimcy@gmail.com>
2020-09-28 10:01:37 +0800

    found the typo bug

--- src/containers/App.js
+++ src/containers/App.js
@@ -32,13 +32,13 @@ const App = () => (
       <ListsContextProvider>
         <ItemsContextProvider>
           <ListsContext.Consumer>
-            {({ lists, loading: listsLoading, error: listsError, getListsRequest, getListRequest })=>(
+            {({ list, lists, loading: listsLoading, error: listsError, getListsRequest, getListRequest })=>(
               <ItemsContext.Consumer>
                 {({ items, loading: itemsLoading, error: itemsError, getItemsRequest }) => (
                   <Switch>
                     <Route exact path='/' render={ props=> lists && <Lists lists={lists} loading={listsLoading} error={listsError} getListsRequest={getListsRequest} {...props} /> } />
                     <Route path='/list/:id/new' component={Form} />
-                    <Route path='/list/:id' render={ props => lists && items && <List lists={lists} listItems={items} loading={itemsLoading} error={itemsError} getListRequest={getListRequest} getItemsRequest={getItemsRequest} {...props}/> } />
+                    <Route path='/list/:id' render={ props => lists && items && <List list={list} items={items} loading={itemsLoading} error={itemsError} getListRequest={getListRequest} getItemsRequest={getItemsRequest} {...props}/> } />
                   </Switch>
                 )}
               </ItemsContext.Consumer>
